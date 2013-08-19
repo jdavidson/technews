35f3c3d7ad47bfe1737c1d4f1a2014b14573708a
Chris Child <thechild@gmail.com>
2013-08-19 10:45:05 -0700

    Added support for comments field

--- read_excel.py
+++ read_excel.py
@@ -17,29 +17,48 @@ def parse_data(excel_data):
     html = companies_to_html(companies)
     return html
 
+def short_comments(name, comments):
+    print "parsing: (%s, %s)" % (name, comments)
+
+    if comments.find(name) == 0:
+        st = comments.replace(name, '')
+        loc = st.find('. ')
+        if loc == -1:
+            loc = st.find('.')
+        loc += len(name) + 1
+    else:
+        loc = comments.find('. ') + 1
+        if loc == 0:
+            loc = comments.find('.') + 1
+    return comments[0:loc]
+
 def companies_in_sheet(sheet):
     companies = []
 
     for row in range(8, sheet.nrows):
-        company = {'name' : sheet.cell(row, 1).value.encode('ascii', errors='ignore').strip(),
+        name = sheet.cell(row, 1).value.encode('ascii', errors='ignore').strip()
+        comments = sheet.cell(row, 7).value.encode('ascii', errors='ignore').strip()
+        company = {'name' : name,
                 'description' : sheet.cell(row, 3).value.encode('ascii', errors='ignore').strip(),
                 'transaction_value' : sheet.cell(row, 4).value,
                 'investors' : sheet.cell(row, 5).value.encode('ascii', errors='ignore').strip(),
-                'long_description' : sheet.cell(row, 6).value.encode('ascii', errors='ignore').strip()}
+                'long_description' : sheet.cell(row, 6).value.encode('ascii', errors='ignore').strip(),
+                'comments' : short_comments(name, comments) }
         companies.append(company)
 
     return companies
 
 def companies_to_html(companies):
-    html = '<table class="table table-striped">\n'
-    html += '<thead>\n<tr>\n<td>Company</td>\n<td>Description</td>\n<td>$</td>\n<td>Investors</td>\n</tr>\n</thead>\n'
+    # html = '<table class="table table-striped">\n'
+    # html += '<thead>\n<tr>\n<td>Company</td>\n<td>Description</td>\n<td>$</td>\n<td>Investors</td>\n<td>Comments</td>\n</tr>\n</thead>\n'
     html = ''
     for company in companies:
-        html += str.format("<tr>\n<td>%s</td>\n<td>%s</td>\n<td style='text-align: center'>%s</td>\n<td>%s</td>\n</tr>\n" %
+        html += str.format("<tr>\n<td>%s</td>\n<td>%s</td>\n<td style='text-align: center'>%s</td>\n<td>%s</td><td>%s</td>\n</tr>\n" %
             (company['name'],
              company['description'],
              str(company['transaction_value']),
-             company['investors']))
+             company['investors'],
+             company['comments']))
 
     return html
 
